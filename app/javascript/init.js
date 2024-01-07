d82635f6cfae97b8ff9b2413695b13850613ae1f
Hoang Nguyen <>
2024-01-07 20:15:36 +0700

    [f] dashboard UI

--- app/javascript/init.js
+++ app/javascript/init.js
@@ -1,6 +1,8 @@
+//= require ./js/optimize/Chart.min
+//= require ./js/optimize/chart_balance
+//= require ./js/optimize/chart_holding
 //= require ./js/optimize/dashboard
 //= require ./js/vendor.bundle.base
-//= require ./js/optimize/Chart.min
 //= require ./js/optimize/off-canvas
 //= require ./js/optimize/hoverable-collapse
 //= require ./js/optimize/misc
