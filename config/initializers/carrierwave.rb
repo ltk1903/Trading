aee3923fc027d10b80826fc39d7edfae2629c531
Hoang Nguyen <>
2024-04-19 08:10:02 +0700

    [f] update git ignore

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ if Rails.env.test? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
-    config.root = "#{Rails.root}/spec/test_files"
+    config.root = "#{Rails.root}/spec/test/files"
   end
 else
   CarrierWave.configure do |config|
